0a06e67e93670115491c7a3e1c429f295f58c20d
Ezequiel <ezequielta63@gmail.com>
2022-09-12 20:39:57 -0500

    actualizado

--- posts.php
+++ posts.php
@@ -21,42 +21,38 @@ $datos=$obj->read();
 
 <article>
 
-
-
-
- 
-    <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
-    <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    </div>
-    
-    <div class="carousel-inner">
-    
-    <?php foreach ($datos as $dato){?>
-
+<div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
    
-        <div class="carousel-item active" data-bs-interval="20000">
-        <img src="crudPosts/imagenes/<?php echo $dato['imagen'] ;?>" class="d-block w-100" alt="...">
-        </div>
-        <?php }; ?>
-        <div class="carousel-caption d-none d-md-block">
-            <h5>First slide label</h5>
-            <p>Some representative placeholder content for the first slide.</p>
-        </div>
+  </div>
+
+  <div class="carousel-inner">
+ 
+  <?php foreach($datos as $dato){ ?>
+    <div class="carousel-item active" data-bs-interval="10000">
+    
+      <img src="crudPosts/imagenes/<?php echo $dato["imagen"] ;?>" class="d-block w-100" alt="..." >
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
      
     </div>
-   
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
-    </div>
+
+    <?php }?>
+  
+  </div>
+ 
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 
 
 
