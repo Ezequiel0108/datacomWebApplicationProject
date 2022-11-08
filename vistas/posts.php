bc488ef14823a2870d9ff0bbd28ffac5007350b0
Ezequiel <ezequielta63@gmail.com>
2022-11-08 14:29:16 -0600

    Logo,contactanos

--- vistas/posts.php
+++ vistas/posts.php
@@ -31,55 +31,74 @@ $datos = $obj->read();
 
 ?>
 
-<body class="azul  " >
+<body class="fondo4 mt-5   " >
   <script src="code.js"></script>
   <header>
-  <nav class="navbar navbar-expand-lg fondo text-white">
-    <div class="container-fluid">
-      <img src="../imagenes/logo.png" class="img-fluid float-start" alt="..." />
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="col-3 bg-warning"></div>
-      <div class="collapse navbar-collapse " id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0 fs-5">
-          <li class="nav-item">
-            <a class=" efecto  btn btn-warning  mx-1 nav-link active fw-bold" href="posts.php">Publicaciones</a>
-          </li>
-          <li class="nav-item">
-            <a class=" efecto  btn btn-warning  mx-1 nav-link active fw-bold" aria-current="page"
-              href="index.html">Inicio</a>
-          </li>
-          <li class="nav-item">
-            <a class=" efecto  btn btn-warning  mx-1 nav-link active fw-bold" href="nosotros.html">Nosotros</a>
-          </li>
-          <li class="nav-item">
-            <a class=" efecto  btn btn-warning  mx-1 nav-link active fw-bold" href="contacto.html">Contacto</a>
-          </li>
-          <li class="nav-item dropdown fs-5">
-            <a class=" efecto btn btn-warning  mx-1 nav-link dropdown-toggle active fw-bold" href="#" role="button"
-              data-bs-toggle="dropdown" aria-expanded="false">
-              Servicios
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+    <!--------------------------------------nav style="background-color: #0a1c26 ;"----------------------------->
+    <!--animate css-->
+    <nav class="navbar  fixed-top navbar-expand-lg bg-light fondo12">
+      <div class="container-fluid">
+        <img src="../imagenes/logo5.png" class=" d-inline-block align-text-top" alt="..." />
+        <button class="navbar-toggler bg-warning" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="col-3 ms-4 bg-warning"></div>
+        <div class="collapse navbar-collapse   mt-4" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0 fs-5">
+            <li class="nav-item dropdown ">
+              <a class=" efecto btn btn-warning  mx-1 nav-link active fw-bold" aria-current="page"
+                href="index.html">Inicio</a>
+              <ul class="dropdown-menu fw-bold  text-center ">
+                <li>Inicio</li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
 
+              <a class="  efecto  btn btn-warning   mx-1 nav-link active fw-bold" href="posts.php">Publicaciones</a>
+              <ul class="dropdown-menu fw-bold text-center">
+                <li>Conoce nuestras publicaciones</li>
+              </ul>
+            </li>
+
+            <li class="nav-item dropdown">
+              <a class=" efecto btn btn-warning  mx-1 nav-link active fw-bold" href="contactanos.html">Contactanos</a>
+              <ul class="dropdown-menu fw-bold text-center">
+                <li>¿Tienes alguna duda?</li>
+              </ul>
+            </li>
+
+
+            <li class="nav-item dropdown fs-5 ">
+              <a class=" efecto btn btn-warning  mx-1 nav-link dropdown-toggle active fw-bold" href="#" role="button"
+                data-bs-toggle="dropdown" aria-expanded="false">
+                Servicios
+              </a>
+              <ul class="dropdown-menu  " >
+                <li><a class="dropdown-item fw-bold" href="#">Action</a></li>
+                <li><a class="dropdown-item fw-bold" href="AccesoBiometrico.html">sistemas de seguridad</a></li>
+                <li><a class="dropdown-item fw-bold" href="#">Action</a></li>
+                <li><a class="dropdown-item fw-bold" href="#">Another action</a></li>
+                <li><a class="dropdown-item fw-bold" href="#">Action</a></li>
+                <li><a class="dropdown-item fw-bold" href="#">Another action</a></li>
+              </ul>
+            </li>
+
+
+
+
+          </ul>
+        </div>
+      </div>
+    </nav>
   </header>
 
-  <article>
+  <article class="mt-5 py-5">
 
-    <section>
+    <section class=">
 
 
-      <div class="margen"></div>
+      <div class="margen "></div>
 
       <div class="row">
         <div class="col-8">
@@ -96,13 +115,13 @@ $datos = $obj->read();
             <!-- Modal -->
             <div class="modal fade " id="modal<?php echo $dato["id"] ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
               <div class="modal-dialog ">
-                <div class="modal-content fondo">
+                <div class="modal-content " style="background-color:black">
                   <div class="modal-header">
-                    <h5 class="modal-title text-black" id="exampleModalLabel">Descripción </h5>
+                    <h5 class="modal-title text-white fw-bold" id="exampleModalLabel">Descripción </h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div class="modal-body">
-                    <b class="text-black"><?php echo $dato["descripcion"];  ?></b>
+                    <b class="text-white fw-bold"><?php echo $dato["descripcion"];  ?></b>
                   </div>
                   <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -119,7 +138,7 @@ $datos = $obj->read();
               <div class="row">
 
                 <div class="col-12 ">
-                  <img class="img-fluid" src="../crudPosts/imagenes/<?php echo $dato["imagen"]; ?> ">
+                  <img class="img-fluid " style=""src="../crudPosts/imagenes/<?php echo $dato["imagen"]; ?> ">
                   <div class="row">
                     <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal<?php echo $dato["id"] ?>">
                       Más detalles
@@ -144,7 +163,7 @@ $datos = $obj->read();
           <br>
           <b class="hit-the-floor  estilo-x "></b>
           <img  class=" img-fluid" src="../imagenes/gif/contactanos2.webp"  alt="">
-          <a class="btn btn-warning ms-4" href="">Comunicate con nostros</a>
+          <a class="btn btn-warning ms-4 text-black fw-bold" href="contactanos.html">Comunicate con nostros</a>
 
         
         </div>
